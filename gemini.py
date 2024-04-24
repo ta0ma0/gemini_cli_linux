9d4aee5cc500174b0f578ee3213b5592ba28bada
ta0ma0 <ruslan.akm@gmail.com>
2024-04-24 09:03:09 +0400

    add rich for pretty formatting

--- gemini.py
+++ gemini.py
@@ -1,27 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # This API tool for query to gemini AI from Google.
 
 # How to use.
-
-# 1. Run script with string query with quotes.
+# 1. API_KEY setup in .bashrc 'export API_KEY='xxxxxxxx' && source .bashrc
+# 2. Run script with string query with quotes.
 
 
 import google.generativeai as genai
 import os
 import argparse
+from rich import print
+from rich.markdown import Markdown
+
+prompt = """You are an assistant administrator and user of the
+Linux system. Next comes the query: \n"""
 
 parser = argparse.ArgumentParser(description='User query')
 parser.add_argument('query', type=str, help='Query')
 args = parser.parse_args()
-
 genai.configure(api_key=os.environ['API_KEY'])
-
 model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content(prompt + args.query)
+response_formated = Markdown(str(response.parts[0].text))
 
-response = model.generate_content(args.query)
+print(response_formated)
 
-print(response.parts)
-#    print(part.content)  # Access the content of each part
-
-#print(response)
