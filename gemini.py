ebfecd266ad4455089647bd8a1adf0a10f7f3df7
Ruslan <ruslan.akm@gmail.com>
2025-02-01 12:12:41 +0400

    change model

--- gemini.py
+++ gemini.py
@@ -17,7 +17,7 @@ args = parser.parse_args()
 
 genai.configure(api_key=os.environ['API_KEY'])
 
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 response = model.generate_content(args.query)
 
